719151d78d49c016e90c7af685b372ab1c46a1a0
joaobiriba <glaquidara@gmail.com>
2014-11-12 17:31:20 +0100

    Sunshing Lesson 4a

--- app/src/main/java/com/laquysoft/sunshine/ForecastFragment.java
+++ app/src/main/java/com/laquysoft/sunshine/ForecastFragment.java
@@ -20,7 +20,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,7 +33,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 
 /**
